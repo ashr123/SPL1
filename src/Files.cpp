53ae14c0b344cc5c0ab200d64b293c313caeaa30
Roy ash <roykid123@gmail.com>
2017-11-09 16:30:45 +0200

    Definitions for all Files classes

--- src/Files.cpp
+++ src/Files.cpp
@@ -5,10 +5,81 @@
 #include "../include/Files.h"
 using namespace std;
 
-File::File(string name, int size): size(size), setName(name){
+File::File(string name, int size) : size(size), setName(name)
+{
+};
+
+int File::getSize()
+{
+    return size;
+};
+
+BaseFile::BaseFile(string name)
+{
 
 }
 
-int File::getSize() {
-    return size;
-};
+string BaseFile::getName() const
+{
+	return std::__cxx11::string();
+}
+
+void BaseFile::setName(string newName)
+{
+
+}
+
+Directory::Directory(string name, Directory *parent)
+{
+
+}
+
+Directory *Directory::getParent() const
+{
+	return nullptr;
+}
+
+void Directory::setParent(Directory *newParent)
+{
+
+}
+
+void Directory::addFile(BaseFile *file)
+{
+
+}
+
+void Directory::removeFile(string name)
+{
+
+}
+
+void Directory::removeFile(BaseFile *file)
+{
+
+}
+
+void Directory::sortByName()
+{
+
+}
+
+void Directory::sortBySize()
+{
+
+}
+
+vector<BaseFile *> Directory::getChildren()
+{
+	return vector<BaseFile *>();
+}
+
+int Directory::getSize()
+{
+	return 0;
+}
+
+string Directory::getAbsolutePath()
+{
+	return std::__cxx11::string();
+}
